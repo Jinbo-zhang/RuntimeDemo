4196da21aa068b8628da87e1490653b92ecd4e09
zhangjb <1094601269@qq.com>
2020-07-02 16:10:15 +0800

    Initial Commit

---
+++ RuntimeDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  RuntimeDemo
+//
+//  Created by zhang jb on 2020/7/2.
+//  Copyright © 2020 tayo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ RuntimeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuntimeDemo
+//
+//  Created by zhang jb on 2020/7/2.
+//  Copyright © 2020 tayo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
